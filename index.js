241a0673c4bf58f9745097c6270abbd7dd3bf8ad
Your Name <ustinovoleksij@gmail.com>
2022-05-23 11:18:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+// let k=0;
+// let count = () => {
+//     for(let i=0;i<1e9; i++) {
+// k++;// Всё, что мы посчитали в i, будет записываться в отдельной переменной
+//     }
+//     alert('Done');
+// }
+//
+//
+// count();
+
+
+setTimeout(()=> {
+    console.log(1);
+},0);
+
+console.log(2);
